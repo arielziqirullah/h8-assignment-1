6a56a2d8b46c07c87f6e48400d3a321a2adcf974
Ariel Ziqirullah <arielmailili@gmail.com>
2023-03-14 22:32:49 +0700

    update interface contract

--- biodata.go
+++ biodata.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
+
+type Biodata interface {
+	Biodata(input string, memberList []string) string
+}
 
 type Person struct {
 	ID        int
@@ -13,8 +18,57 @@ type Person struct {
 	Alasan    string
 }
 
-func (p Person) Biodata() string {
-	return fmt.Sprintf("\n ID : %d \n nama : %s \n alamat : %s \n pekerjaan : %s \n alasan : %s", p.ID, p.Name, p.Alamat, p.Pekerjaan, p.Alasan)
+func (p Person) Biodata(input string, memberList []string) Person {
+
+	switch strings.ToLower(input) {
+	case "budi":
+		person := Person{
+			ID:        1,
+			Name:      "Budi",
+			Alamat:    "Jakarta",
+			Pekerjaan: "Developer",
+			Alasan:    "Mencari pengalaman baru",
+		}
+		return person
+	case "thomas":
+		person := Person{
+			ID:        2,
+			Name:      "Thomas",
+			Alamat:    "Jakarta",
+			Pekerjaan: "Developer",
+			Alasan:    "Mencari pengalaman belajar golang",
+		}
+		return person
+	case "rahmat":
+		person := Person{
+			ID:        3,
+			Name:      "Rahmat",
+			Alamat:    "Bandung",
+			Pekerjaan: "Mobile Developer",
+			Alasan:    "Belajar bahasa pemrograman baru",
+		}
+		return person
+	case "rizal":
+		person := Person{
+			ID:        4,
+			Name:      "Rizal",
+			Alamat:    "Surabaya",
+			Pekerjaan: "Front Developer",
+			Alasan:    "Belajat golang",
+		}
+		return person
+	case "rudi":
+		person := Person{
+			ID:        5,
+			Name:      "Rudi",
+			Alamat:    "Jakarta",
+			Pekerjaan: "Backend Developer",
+			Alasan:    "Belajar bahasa backend yang lain",
+		}
+		return person
+	default:
+		return Person{}
+	}
 }
 
 func main() {
@@ -23,17 +77,12 @@ func main() {
 
 	memberList := []string{"Budi", "Thomas", "Rahmat", "Rizal", "Rudi"}
 
-	for i, member := range memberList {
-		if input == member {
-			person := Person{
-				ID:        i,
-				Name:      input,
-				Alamat:    "Jakarta",
-				Pekerjaan: "Developer",
-				Alasan:    "Mencari pengalaman baru",
-			}
-			fmt.Println(person.Biodata())
-		}
+	person := Person{}
+	process := person.Biodata(input, memberList)
+	if len(process.Name) == 0 {
+		fmt.Println("Data tidak ditemukan")
+		os.Exit(1)
 	}
-
+	result := fmt.Sprintf("\n ID : %d \n nama : %s \n alamat : %s \n pekerjaan : %s \n alasan : %s", process.ID, process.Name, process.Alamat, process.Pekerjaan, process.Alasan)
+	fmt.Println(result)
 }
